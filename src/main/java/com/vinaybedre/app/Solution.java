4b6f4e3c5ed404ba9fe9db19cf0c3b2faca8a3b1
Vinay Bedre <vinay.bedre@cision.com>
2019-09-30 12:05:03 +0200

    added two sum

--- src/main/java/com/vinaybedre/app/Solution.java
+++ src/main/java/com/vinaybedre/app/Solution.java
@@ -1,67 +1,85 @@
 package com.vinaybedre.app;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Solution {
     // https://leetcode.com/problems/string-to-integer-atoi/
     public int myAtoi(String str) {
         int index;
-        char currentChar=' ';
+        char currentChar = ' ';
         boolean isNegative = false;
-        long resultNumber=0;
-        for(index=0;index<str.length();index++){
+        long resultNumber = 0;
+        for (index = 0; index < str.length(); index++) {
             currentChar = str.charAt(index);
-            if(currentChar!=' '){
-                if(currentChar=='-'){
+            if (currentChar != ' ') {
+                if (currentChar == '-') {
                     isNegative = true;
                     index++;
                 }
-                if(currentChar=='+'){
+                if (currentChar == '+') {
                     index++;
                 }
                 break;
             }
         }
 
-        for(;index<str.length();index++){
+        for (; index < str.length(); index++) {
             currentChar = str.charAt(index);
-            if(Character.isDigit(currentChar)){
+            if (Character.isDigit(currentChar)) {
                 resultNumber *= 10;
                 resultNumber += Character.getNumericValue(currentChar);
-            }
-            else{
+            } else {
                 break;
             }
 
-            if(resultNumber>Integer.MAX_VALUE){
-                if(isNegative){
+            if (resultNumber > Integer.MAX_VALUE) {
+                if (isNegative) {
                     return Integer.MIN_VALUE;
                 }
                 return Integer.MAX_VALUE;
             }
         }
 
-        resultNumber = isNegative?-resultNumber:resultNumber;
+        resultNumber = isNegative ? -resultNumber : resultNumber;
 
-        return (int)resultNumber;
+        return (int) resultNumber;
     }
 
     //https://leetcode.com/problems/palindrome-number/
     public boolean isPalindrome(int x) {
         int originalInput = x;
-        if(x<0){
+        if (x < 0) {
             return false;
         }
 
-        int revNumber=0;
+        int revNumber = 0;
 
-        while(x!=0){
+        while (x != 0) {
             revNumber *= 10;
-            revNumber += x%10;
-            x = x/10;
+            revNumber += x % 10;
+            x = x / 10;
         }
 
-        if(revNumber==originalInput){
+        if (revNumber == originalInput) {
             return true;
         }
         return false;
+    }
+
+    //https://leetcode.com/problems/two-sum/
+    public int[] twoSum(int[] nums, int target) {
+        //operand and index
+        Map<Integer, Integer> possibleOperands = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (possibleOperands.containsKey(nums[i])) {
+                return new int[]{possibleOperands.get(nums[i]), i};
+            }
+            int diff = target - nums[i];
+            possibleOperands.put(diff, i);
+        }
+        return new int[2];
     }
 }
--- src/test/java/com/vinaybedre/app/SolutionTest.java
+++ src/test/java/com/vinaybedre/app/SolutionTest.java
@@ -1,6 +1,10 @@
 package com.vinaybedre.app;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -11,19 +15,31 @@ public class SolutionTest {
     Solution solution = new Solution();
 
     @Test
-    public void myAtoi(){
-        Assert.assertEquals(42,solution.myAtoi("42"));
-        Assert.assertEquals(-42,solution.myAtoi("   -42"));
-        Assert.assertEquals(42,solution.myAtoi("   +42"));
-        Assert.assertEquals(4193,solution.myAtoi("4193 with words"));
-        Assert.assertEquals(0,solution.myAtoi("words and 987"));
-        Assert.assertEquals(-2147483648,solution.myAtoi("-91283472332"));
+    public void myAtoi() {
+        assertEquals(42, solution.myAtoi("42"));
+        assertEquals(-42, solution.myAtoi("   -42"));
+        assertEquals(42, solution.myAtoi("   +42"));
+        assertEquals(4193, solution.myAtoi("4193 with words"));
+        assertEquals(0, solution.myAtoi("words and 987"));
+        assertEquals(-2147483648, solution.myAtoi("-91283472332"));
     }
 
     @Test
-    public void isPalindrome(){
-        Assert.assertEquals(true,solution.isPalindrome(121));
-        Assert.assertEquals(false,solution.isPalindrome(-121));
-        Assert.assertEquals(false,solution.isPalindrome(10));
+    public void isPalindrome() {
+        assertTrue(solution.isPalindrome(121));
+        assertFalse(solution.isPalindrome(-121));
+        assertFalse(solution.isPalindrome(10));
+    }
+
+    @Test
+    public void twoSum() {
+        int nums[] = new int[]{3, 4};
+        assertArrayEquals(new int[]{0, 1}, solution.twoSum(nums, 7));
+
+        nums = new int[]{2, 7, 11, 15};
+        assertArrayEquals(new int[]{0, 1}, solution.twoSum(nums, 9));
+
+        nums = new int[]{2, -7, 11, -15};
+        assertArrayEquals(new int[]{1, 3}, solution.twoSum(nums, -22));
     }
 }
